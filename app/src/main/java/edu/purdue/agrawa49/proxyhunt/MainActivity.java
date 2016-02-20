b5eda8675df85d71ea0f64c024664b3f894e3718
agrawa49 <agrawa49@purdue.edu>
2016-02-20 12:32:11 -0600

    firebase

--- app/src/main/java/edu/purdue/agrawa49/proxyhunt/MainActivity.java
+++ app/src/main/java/edu/purdue/agrawa49/proxyhunt/MainActivity.java
@@ -8,11 +8,14 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.firebase.client.Firebase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Firebase.setAndroidContext(this);
         setContentView(R.layout.activity_main);
 
         Button b1 = (Button) findViewById(R.id.accept);
