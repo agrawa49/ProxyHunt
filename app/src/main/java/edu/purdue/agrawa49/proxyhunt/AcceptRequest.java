10b145fa7477c8ba5efaf52f511e1ed0cb0f5e33
Rohan Kashuka <rkashuka@purdue.edu>
2016-02-20 21:09:58 -0600

    bleh

--- app/src/main/java/edu/purdue/agrawa49/proxyhunt/AcceptRequest.java
+++ app/src/main/java/edu/purdue/agrawa49/proxyhunt/AcceptRequest.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import java.util.ArrayList;
@@ -23,6 +24,11 @@ import android.widget.ExpandableListView.OnGroupCollapseListener;
 import android.widget.ExpandableListView.OnGroupExpandListener;
 import android.widget.Toast;
 
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
 public class AcceptRequest extends AppCompatActivity {
 
 //    @Override
@@ -35,60 +41,84 @@ public class AcceptRequest extends AppCompatActivity {
     static List<String> listDataHeader;
     static HashMap<String, List<String>> listDataChild;
 
+    private static final String Firebase_url = "https://blinding-fire-6276.firebaseio.com";
+    private Firebase firebaseRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accept_request);
+        firebaseRef = new Firebase(Firebase_url);
+    }
+
+    public  void prepareListData() {
+        firebaseRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+              //  System.out.println("There are " + snapshot.getChildrenCount() + " blog posts");
+                for (DataSnapshot postSnapshot: snapshot.getChildren()) {
+                    SendInfo info = postSnapshot.getValue(SendInfo.class);
+                }
+            }
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                System.out.println("The read failed: " + firebaseError.getMessage());
+            }
+        });
+    }
+
+    EditText e1 = (EditText) findViewById(R.id.cv);
+    public void updateList() {
+
+    }
 
         // get the listview
-        expListView = (ExpandableListView) findViewById(R.id.lvExp);
+        // expListView = (ExpandableListView) findViewById(R.id.lvExp);
 
         // preparing list data
         //prepareListData();
 
-        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
+        // listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
 
         // setting list adapter
-        expListView.setAdapter(listAdapter);
+     //   expListView.setAdapter(listAdapter);
     }
-
     /*
      * Preparing the list data
      */
-    public static void prepareListData(String c, String t, String l) {
-        listDataHeader = new ArrayList<String>();
-        listDataChild = new HashMap<String, List<String>>();
-
-        // Adding child data
-        listDataHeader.add(c);
-        // Adding child data
-        List<String> x = new ArrayList<String>();
-        x.add(t);
-//        top250.add("The Godfather");
-//        top250.add("The Godfather: Part II");
-//        top250.add("Pulp Fiction");
-//        top250.add("The Good, the Bad and the Ugly");
-//        top250.add("The Dark Knight");
-//        top250.add("12 Angry Men");
+//    public static void prepareListData(String c, String t, String l) {
+//        listDataHeader = new ArrayList<String>();
+//        listDataChild = new HashMap<String, List<String>>();
 //
-//        List<String> nowShowing = new ArrayList<String>();
-//        nowShowing.add("The Conjuring");
-//        nowShowing.add("Despicable Me 2");
-//        nowShowing.add("Turbo");
-//        nowShowing.add("Grown Ups 2");
-//        nowShowing.add("Red 2");
-//        nowShowing.add("The Wolverine");
+//        // Adding child data
+//        listDataHeader.add(c);
+//        // Adding child data
+//        List<String> x = new ArrayList<String>();
+//        x.add(t);
+////        top250.add("The Godfather");
+////        top250.add("The Godfather: Part II");
+////        top250.add("Pulp Fiction");
+////        top250.add("The Good, the Bad and the Ugly");
+////        top250.add("The Dark Knight");
+////        top250.add("12 Angry Men");
+////
+////        List<String> nowShowing = new ArrayList<String>();
+////        nowShowing.add("The Conjuring");
+////        nowShowing.add("Despicable Me 2");
+////        nowShowing.add("Turbo");
+////        nowShowing.add("Grown Ups 2");
+////        nowShowing.add("Red 2");
+////        nowShowing.add("The Wolverine");
+////
+////        List<String> comingSoon = new ArrayList<String>();
+////        comingSoon.add("2 Guns");
+////        comingSoon.add("The Smurfs 2");
+////        comingSoon.add("The Spectacular Now");
+////        comingSoon.add("The Canyons");
+////        comingSoon.add("Europa Report");
 //
-//        List<String> comingSoon = new ArrayList<String>();
-//        comingSoon.add("2 Guns");
-//        comingSoon.add("The Smurfs 2");
-//        comingSoon.add("The Spectacular Now");
-//        comingSoon.add("The Canyons");
-//        comingSoon.add("Europa Report");
+//        listDataChild.put(listDataHeader.get(0), x); // Header, Child data
+////        listDataChild.put(listDataHeader.get(1), nowShowing);
+////        listDataChild.put(listDataHeader.get(2), comingSoon);
+//    }
 
-        listDataChild.put(listDataHeader.get(0), x); // Header, Child data
-//        listDataChild.put(listDataHeader.get(1), nowShowing);
-//        listDataChild.put(listDataHeader.get(2), comingSoon);
-    }
-
-    }
